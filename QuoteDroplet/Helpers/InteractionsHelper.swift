e7af29a1e1348efcd527486a771cffd09000936d
Daggerpov <danielagapov1@gmail.com>
2024-07-21 19:21:30 -0700

    Added InfoView with about me section + reqReview() method to just request review from within infoview

--- QuoteDroplet/Helpers/InteractionsHelper.swift
+++ QuoteDroplet/Helpers/InteractionsHelper.swift
@@ -17,11 +17,17 @@ func interactionsIncrease(from: String = "within app") {
     @AppStorage("interactions", store: UserDefaults(suiteName: "group.selectedSettings"))
     var interactions = 0
     
-    @Environment(\.requestReview) var requestReview
-    
     interactions += 1
     if (interactions == 21 && from != "widget") {
         // within app, so review should show
+        reqReview(from: "interactions")
+    }
+}
+
+@MainActor @available(iOS 16.0, *)
+func reqReview(from: String = "nowhere special") {
+    @Environment(\.requestReview) var requestReview
+    if (from == "InfoView" || from == "interactions") {
         requestReview()
     }
 }
---
+++ QuoteDroplet/Views/InfoView.swift
@@ -0,0 +1,131 @@
+//
+//  InfoView.swift
+//  Quote Droplet
+//
+//  Created by Daniel Agapov on 2024-07-21.
+//
+
+import SwiftUI
+import WidgetKit
+import UserNotifications
+import UIKit
+import Foundation
+import StoreKit
+
+struct InfoView: View {
+    @EnvironmentObject var sharedVars: SharedVarsBetweenTabs
+    
+    @AppStorage("widgetColorPaletteIndex", store: UserDefaults(suiteName: "group.selectedSettings"))
+    var widgetColorPaletteIndex = 0
+    
+    // actual colors of custom:
+    @AppStorage("widgetCustomColorPaletteFirstIndex", store: UserDefaults(suiteName: "group.selectedSettings"))
+    private var widgetCustomColorPaletteFirstIndex = "1C7C54"
+    
+    @AppStorage("widgetCustomColorPaletteSecondIndex", store: UserDefaults(suiteName: "group.selectedSettings"))
+    private var widgetCustomColorPaletteSecondIndex = "E2B6CF"
+    
+    @AppStorage("widgetCustomColorPaletteThirdIndex", store: UserDefaults(suiteName: "group.selectedSettings"))
+    private var widgetCustomColorPaletteThirdIndex = "DEF4C6"
+    
+    private var aboutMeSection: some View {
+        HStack {
+            Text("Contact:")
+                .font(.title2)
+                .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[1] ?? .white)
+                .padding(.leading, 10)
+            
+            Spacer()
+            
+            Link(destination: URL(string: "https://www.linkedin.com/in/danielagapov/")!) {
+                Image("linkedinlogo")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[1] ?? .white)
+            }
+            
+            Spacer()
+            
+            Link(destination: URL(string: "https://github.com/Daggerpov")!) {
+                Image("githublogo")
+                    .resizable()
+                    .frame(width: 50, height: 50)
+                    .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[1] ?? .white)
+            }
+            
+            Spacer()
+            
+            Link(destination: URL(string: "mailto:danielagapov1@gmail.com?subject=Quote%20Droplet%20Contact")!) {
+                Image("gmaillogo")
+                    .resizable()
+                    .frame(width: 60, height: 50)
+                    .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[1] ?? .white)
+            }
+            
+            Spacer()
+        }
+        
+        .padding(EdgeInsets(top: 10, leading: 0, bottom: 10, trailing: 0))
+        .background(ColorPaletteView(colors: [colorPalettes[safe: sharedVars.colorPaletteIndex]?[0] ?? Color.clear]))
+        .cornerRadius(20)
+        .shadow(radius: 5)
+        .padding(.horizontal)
+    }
+    
+    private var reviewButton: some View {
+        Button(action: {
+            if #available(iOS 16.0, *) {
+                reqReview(from: "InfoView")
+            } else {
+                // Fallback on earlier versions
+            }
+        }) {
+            HStack {
+                Text("Rate Quote Droplet")
+                    .font(.headline)
+                    .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[1] ?? .blue)
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 30, height: 30)
+                    .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[1] ?? .blue)
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(colorPalettes[safe: sharedVars.colorPaletteIndex]?[2] ?? .blue, lineWidth: 2)
+            )
+        }
+        .padding()
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                AdBannerViewController(adUnitID: "ca-app-pub-5189478572039689/9761642936") // TODO: change adUnitID to new one, maybe
+                    .frame(height: 60)
+                HStack {
+                    Spacer()
+                    Text("App Info")
+                        .font(.title)
+                        .foregroundColor(colorPalettes[safe: sharedVars.colorPaletteIndex]?[2] ?? .blue)
+                        .padding(.bottom, 5)
+                    
+                    Spacer()
+                }
+                reviewButton
+                aboutMeSection
+            }
+            .frame(maxWidth: .infinity)
+            .background(ColorPaletteView(colors: [colorPalettes[safe: sharedVars.colorPaletteIndex]?[0] ?? Color.clear]))
+            .onAppear {
+                // Fetch initial quotes when the view appears
+                sharedVars.colorPaletteIndex = widgetColorPaletteIndex
+                
+                colorPalettes[3][0] = Color(hex: widgetCustomColorPaletteFirstIndex)
+                colorPalettes[3][1] = Color(hex: widgetCustomColorPaletteSecondIndex)
+                colorPalettes[3][2] = Color(hex: widgetCustomColorPaletteThirdIndex)
+            }
+        }
+    }
+}
