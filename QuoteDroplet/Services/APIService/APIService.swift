65a7d2840c8fd8fc9b5edc5c99147703bff92300
Daniel Agapov <danielagapov1@gmail.com>
2024-10-29 17:51:04 -0700

    Utilizing `makeGetRequest()`

--- QuoteDroplet/Services/APIService/APIService.swift
+++ QuoteDroplet/Services/APIService/APIService.swift
@@ -40,136 +40,39 @@ class APIService: IAPIService {
         }.resume()
     }
     
-    func getQuotesByAuthor(author: String, completion: @escaping ([Quote]?, Error?) -> Void) {
-        let urlString = "\(baseUrl)/quotes/author=\(author)"
-        
-        guard let url = URL(string: urlString) else {
-            completion(nil, NSError(domain: "InvalidURL", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
+    func getRandomQuoteByClassification(classification: String, completion: @escaping (Quote?, Error?) -> Void, isShortQuoteDesired: Bool = false) {
+        var urlString = "\(baseUrl)/quotes"
+        if classification != "all" {
+            urlString += "/classification=\(classification)"
+        }
+        if isShortQuoteDesired {
+            urlString += "/maxQuoteLength=65"
         }
         
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                if let httpResponse = response as? HTTPURLResponse {
-                    completion(nil, NSError(domain: "HTTPError", code: httpResponse.statusCode, userInfo: nil))
-                } else {
-                    completion(nil, NSError(domain: "HTTPError", code: -1, userInfo: nil))
-                }
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, NSError(domain: "NoDataError", code: -1, userInfo: nil))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let quotes = try decoder.decode([Quote].self, from: data)
-                completion(quotes, nil)
-            } catch {
+        makeGetRequest(urlString: urlString) { (quotes: [Quote]?, error) in
+            if let quotes = quotes, !quotes.isEmpty {
+                let randomQuote = quotes.randomElement() ?? Quote(id: -1, text: "No Quote Found.", author: nil, classification: nil, likes: 0)
+                completion(randomQuote, nil)
+            } else {
                 completion(nil, error)
             }
-        }.resume()
+        }
+    }
+    
+    func getQuotesByAuthor(author: String, completion: @escaping ([Quote]?, Error?) -> Void) {
+        let urlString = "\(baseUrl)/quotes/author=\(author)"
+        makeGetRequest(urlString: urlString, completion: completion)
     }
     
     func getQuotesBySearchKeyword(searchKeyword: String, searchCategory: String, completion: @escaping ([Quote]?, Error?) -> Void) {
         let urlString = "\(baseUrl)/admin/search/\(searchKeyword)?category=\(searchCategory)"
-        print("URL STRING:")
-        print(urlString)
-        
-        guard let url = URL(string: urlString) else {
-            completion(nil, NSError(domain: "InvalidURL", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                if let httpResponse = response as? HTTPURLResponse {
-                    completion(nil, NSError(domain: "HTTPError", code: httpResponse.statusCode, userInfo: nil))
-                } else {
-                    completion(nil, NSError(domain: "HTTPError", code: -1, userInfo: nil))
-                }
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, NSError(domain: "NoDataError", code: -1, userInfo: nil))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let quotes = try decoder.decode([Quote].self, from: data)
-                completion(quotes, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
+        makeGetRequest(urlString: urlString, completion: completion)
     }
     
     func getRecentQuotes(limit: Int, completion: @escaping ([Quote]?, Error?) -> Void) {
         let urlString = "\(baseUrl)/quotes/recent/\(limit)"
-        guard let url = URL(string: urlString) else {
-            completion(nil, NSError(domain: "InvalidURL", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                if let httpResponse = response as? HTTPURLResponse {
-                    completion(nil, NSError(domain: "HTTPError", code: httpResponse.statusCode, userInfo: nil))
-                } else {
-                    completion(nil, NSError(domain: "HTTPError", code: -1, userInfo: nil))
-                }
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, NSError(domain: "NoDataError", code: -1, userInfo: nil))
-                return
-            }
-            
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                let quotes = try decoder.decode([Quote].self, from: data)
-                completion(quotes, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
+        makeGetRequest(urlString: urlString, completion: completion)
     }
-    
     
     func addQuote(text: String, author: String?, classification: String, completion: @escaping (Bool, Error?) -> Void) {
         let urlString = "\(baseUrl)/quotes"
@@ -229,40 +132,7 @@ class APIService: IAPIService {
     
     func likeQuote(quoteID: Int, completion: @escaping (Quote?, Error?) -> Void) {
         let urlString = "\(baseUrl)/quotes/like/\(quoteID)"
-        guard let url = URL(string: urlString) else {
-            completion(nil, NSError(domain: "InvalidURL", code: -1, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
-        }
-        
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode),
-                  let data = data else {
-                if let httpResponse = response as? HTTPURLResponse {
-                    completion(nil, NSError(domain: "HTTPError", code: httpResponse.statusCode, userInfo: nil))
-                } else {
-                    completion(nil, NSError(domain: "HTTPError", code: -1, userInfo: nil))
-                }
-                return
-            }
-            
-            // Parse the JSON response to get the updated quote
-            do {
-                let updatedQuote = try JSONDecoder().decode(Quote.self, from: data)
-                completion(updatedQuote, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
+        makePostRequest(urlString: urlString, completion: completion)
     }
     
     func unlikeQuote(quoteID: Int, completion: @escaping (Quote?, Error?) -> Void) {
@@ -298,61 +168,20 @@ class APIService: IAPIService {
     }
     
     func getQuoteByID(id: Int, completion: @escaping (Quote?, Error?) -> Void) {
-        guard let url = URL(string: "\(baseUrl)/quotes/\(id)") else {
-            completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"]))
-            return
-        }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                completion(nil, error)
-                return
-            }
-            
-            guard let data = data else {
-                completion(nil, NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"]))
-                return
-            }
-            
-            do {
-                let quote = try JSONDecoder().decode(Quote.self, from: data)
-                completion(quote, nil)
-            } catch {
-                completion(nil, error)
-            }
-        }.resume()
+        makeGetRequest(urlString: "\(baseUrl)/quotes/\(id)", completion: completion)
     }
     
     func getLikeCountForQuote(quoteGiven: Quote, completion: @escaping (Int) -> Void) {
-        guard let url = URL(string: "\(baseUrl)/quoteLikes/\(quoteGiven.id)") else {
-            completion(0)
-            return
+        makeGetRequest(urlString: "\(baseUrl)/quoteLikes/\(quoteGiven.id)") { (response: [String: Any]?, _) in
+            let likeCount = response?["likes"] as? Int ?? 0
+            completion(likeCount)
         }
-        
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data,
-               let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-               let likeCount = json["likes"] as? Int {
-                completion(likeCount)
-            } else {
-                completion(0)
-            }
-        }.resume()
     }
     
     func getCountForCategory(category: QuoteCategory, completion: @escaping (Int) -> Void) {
-        guard let url = URL(string: "\(baseUrl)/quoteCount?category=\(category.rawValue.lowercased())") else {
-            completion(0)
-            return
+        makeGetRequest(urlString: "\(baseUrl)/quoteCount?category=\(category.rawValue.lowercased())") { (response: [String: Any]?, _) in
+            let count = response?["count"] as? Int ?? 0
+            completion(count)
         }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let data = data,
-               let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-               let count = json["count"] as? Int {
-                completion(count)
-            } else {
-                completion(0)
-            }
-        }.resume()
     }
 }
